07c5c714bd5759e4e9fa80910a052cc92dd0f668
Divya Raj <divyaraj7612@gmail.com>
2023-02-22 19:42:28 +0530

    create mainpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Header/Navbar';
+import Mainpage from './components/Mainpage/Mainpage';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Mainpage />
 
     </div>
   );
---
+++ src/components/Mainpage/Mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Mainpage.css"
+
+const Mainpage = () => {
+  return (
+    <div className='mainPageContainer'>
+      <div className='mainPageImageContainer'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Mainpage
